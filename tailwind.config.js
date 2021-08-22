64c4e603047ee01551857a4eb370fe45e8c777cb
Prince Radebe <princeradebe@gmail.com>
2021-08-22 15:02:51 +0200

    Adds Header and Services Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ["./public/**/*.{html.js}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat"],
+      },
+      backgroundImage: (theme) => ({
+        "hero-pattern": "url('/public/assets/bg.jpg')",
+      }),
+    },
   },
   variants: {
     extend: {},
